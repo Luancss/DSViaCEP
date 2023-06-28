70a98c7be03c0c299bfb76cc1e2a6de52901776f
Luancss <ventobo123@gmail.com>
2023-06-28 16:31:26 -0300

    Criando Card

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -1,5 +1,6 @@
 import Address from "../models/address.js";
 import * as addressService from '../services/address-service.js';
+import * as listController from './list-controller.js';
 
 function State() {
   this.address = new Address();
@@ -64,6 +65,7 @@ async function handleInputCepChange(event) {
 
 async function handleBtnSaveClick(event) {
   event.preventDefault();
+  listController.addCard(state.address);
 }
 
 function handleInputNumberChange (event) {
