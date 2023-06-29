db5a6772f705d5a1169a361a1223374af42a1f0c
Luancss <ventobo123@gmail.com>
2023-06-29 01:34:06 -0300

    Projeto Finalizado

---
+++ js/controllers/modal-controller.js
@@ -0,0 +1,36 @@
+function State() {
+  this.container = null;
+  this.btnClose = null;
+}
+
+const state = new State();
+
+export function init() {
+  state.container = document.querySelector("#modal-contact");
+  state.btnClose = document.querySelector("#modal-contact-close");
+
+  state.btnClose.addEventListener('click', handleBtnCloseClick);
+  state.container.addEventListener('click', handleContainerClick);
+}
+
+export function showModal() {
+  state.container.classList.add("active");
+}
+
+export function closeModal() {
+  state.container.classList.remove("active");
+}
+
+function handleBtnCloseClick(event) {
+  event.preventDefault();
+  closeModal();
+}
+
+function handleContainerClick(event) {
+  event.preventDefault();
+  if(event.target === this) {
+    closeModal();
+  }
+}
+  
+  
--- js/dsviacep.js
+++ js/dsviacep.js
@@ -1,5 +1,9 @@
 import * as formController from './controllers/form-controller.js';
 import * as listController from './controllers/list-controller.js';
+import * as modalController from './controllers/modal-controller.js';
+import * as pageController from './controllers/page-controller.js';
 
+pageController.init();
+modalController.init();
 formController.init();
 listController.init();
